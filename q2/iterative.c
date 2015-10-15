bb56ab7a1027e530ee19f3f34e262c0343a4ab35
lin11220206 <lin11220206@gmail.com>
2015-10-16 05:20:02 +0800

    First commit q2
    implement iterative and recursive version of q2
    lack of the function of reading test file

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,22 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+
+char smallest_character(char str[], char c);
 
 int main()
 {
+    char str[] = "cfjpv";
+    printf("%c\n", smallest_character(str, 'c'));
     return 0;
+}
+
+char smallest_character(char str[], char c)
+{
+    int i = 0;
+    while(str[i] != '\0') {
+        if(str[i] > c)
+            return str[i];
+        i++;
+    }
+    return str[0];
 }
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,28 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+
+char smallest_character(char str[], char c);
 
 int main()
 {
+    char str[] = "cfjpv";
+    printf("%c\n", smallest_character(str, 'a'));
     return 0;
+}
+
+char smallest_character(char str[], char c)
+{
+    char nextChar;
+
+    if(str[0] == '\0')	return 0;
+
+    if(str[0] > c)
+        return str[0];
+    else {
+        nextChar = smallest_character(str+1, c);
+        if(nextChar <= c)
+            return str[0];
+
+        return nextChar;
+    }
 }
