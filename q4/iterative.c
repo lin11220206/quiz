47db5bbd805aaea3207ff9c15d8650cba8e7bd82
lin11220206 <lin11220206@gmail.com>
2015-10-16 06:07:40 +0800

    First commit the iterative version of q4
    implement iterative version of q4
    lack of the function to reading test file

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,25 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+
+#define MAX(a,b) (a>b?a:b)
+
+int maxSubArray(int A[], int n);
 
 int main()
 {
+    int a[10] = {-2,1,-1,4,-1,2,1,-5,4};
+    printf("%d\n", maxSubArray(a, 10));
     return 0;
+}
+
+int maxSubArray(int A[], int n)
+{
+    int sum = 0;
+    int maxNumber = A[0];
+    for(int i=0; i<n; ++i) {
+        sum += A[i];
+        sum = MAX(0, sum);
+        maxNumber = MAX(sum, maxNumber);
+    }
+    return maxNumber;
 }
