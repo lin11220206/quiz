4696aa9f2336eeaa1ad72f6bb8b4a810be088e5c
lin11220206 <lin11220206@gmail.com>
2015-10-18 17:14:53 +0800

    Add the function to test efficiency

--- q27/main.c
+++ q27/main.c
@@ -1,22 +1,50 @@
 /* FIXME: Implement! */
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 int *getCards();
 
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
+
+
 int main()
 {
-    int *cards = getCards();
+	double cpu_time;
+	struct timespec start, end;	
+	int *cards;	
 
-    for(int i=0; i<52; ++i)
-        printf("%d ", cards[i]);
-    printf("\n");
+	clock_gettime(CLOCK_REALTIME, &start);
+    int count = 10000;
+	srand(time(NULL));
+    while(count--) {
+		cards = getCards();
+		/*
+        for(int i=0; i<52; ++i)
+            printf("%d ", cards[i]);
+        printf("\n");*/
+    }
+	clock_gettime(CLOCK_REALTIME, &end);
+	cpu_time = diff_in_second(start, end);
+	
+	printf("execution time of getCard(): %lf sec\n", cpu_time);
+
     return 0;
 }
 
 int *getCards()
 {
-    srand(time(NULL));
     int *cards = malloc(sizeof(int)*52);
     int decks[52];
     int decksize = 52;
